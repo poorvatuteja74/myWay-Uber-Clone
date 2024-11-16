0c135f5cb2df958cbd70718dda77dfe29b11bdd2
poorvatuteja74 <168651056+poorvatuteja74@users.noreply.github.com>
2024-11-16 05:30:57 -0500

    different tab pages created

---
+++ app/(root)/(tabs)/chat.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native";
+
+const Chat = () => {
+  return (
+    <SafeAreaView>
+      <Text>Chat</Text>
+    </SafeAreaView>
+  );
+};
+export default Chat;
---
+++ app/(root)/(tabs)/rides.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native";
+
+const Rides = () => {
+  return (
+    <SafeAreaView>
+      <Text>Ride</Text>
+    </SafeAreaView>
+  );
+};
+export default Rides;
