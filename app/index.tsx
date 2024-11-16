2f32a36c58b74a9398d9561442f08856483a0f88
poorvatuteja74 <168651056+poorvatuteja74@users.noreply.github.com>
2024-11-16 05:17:13 -0500

    Home Page created

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+    </SafeAreaView>
+  );
+};
+export default Home;
